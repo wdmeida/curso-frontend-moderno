9b18b2bffaa1d28659c83dcc195e1c2fbe9a43e5
Wagner Almeida <wdmeida@gmail.com>
2017-11-16 19:37:04 -0200

    Adding transiction in menu opened and tag aria to better usabolity

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -3,7 +3,15 @@
   
   var btn = document.querySelector('.header-nav__hamburgger');
   var html = document.querySelector('html');
+  var menu = document.querySelector('#mainMenu');
+  var classMenu = 'menu-opened';
   var menuOpened = false;
+
+  html.addEventListener('click', function(e) {
+    if (e.target === html && menuOpened) {
+      closeMenu();
+    }
+  })
 
   btn.addEventListener('click', toggleMenu);
 
@@ -17,11 +25,16 @@
 
   function closeMenu() {
     menuOpened = false;
-    html.classList.remove('menu-opened');
+    html.classList.remove(classMenu);
+    btn.blur();
+    menu.setAttribute('aria-expanded', false);
+    btn.setAttribute('aria-expanded', false);    
   }
 
   function openMenu() {
     menuOpened = true;
-    html.classList.add('menu-opened');
+    html.classList.add(classMenu);
+    menu.setAttribute('aria-expanded', true);
+    btn.setAttribute('aria-expanded', true);
   }
 }())
