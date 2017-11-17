eba1ec10593efd633db0a22320d4805329e28db6
Wagner Almeida <wdmeida@gmail.com>
2017-11-17 21:55:49 -0200

    Adding templates to components footer and nav

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    include = require('gulp-file-include'),
     browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
@@ -8,10 +9,16 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('./src/css'));
 })
 
+gulp.task('html', function() {
+  gulp.src('./src/index.html')
+      .pipe(include())
+      .pipe(gulp.dest('./dist/'))
+})
+
 gulp.task('server', function() {
   browserSync.init({
     server: {
-      baseDir: 'src'
+      baseDir: 'dist'
     }
   })
 
