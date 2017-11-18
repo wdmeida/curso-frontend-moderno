10376e9fcbb5a44f078267a924b00548dd83ef39
Wagner Almeida <wdmeida@gmail.com>
2017-11-17 23:47:38 -0200

    Extract cards to components and create new gulp tasks (clear, copy and html)

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     include = require('gulp-file-include'),
+    clean = require('gulp-clean'),
     browserSync = require('browser-sync');
 
-gulp.task('copy', function() {
+gulp.task('clean', function() {
+  // stream
+  return gulp.src('dist')
+             .pipe(clean())
+})
+
+gulp.task('copy', ['clean'], function() {
   gulp.src([
     'src/components/**/*',
     'src/css/**/*',
@@ -16,23 +23,24 @@ gulp.task('copy', function() {
 gulp.task('sass', function() {
   gulp.src('./src/sass/**/*.scss')
       .pipe(sass())
-      .pipe(gulp.dest('./src/css'));
+      .pipe(gulp.dest('./dist/css'));
 })
 
 gulp.task('html', function() {
-  gulp.src('./src/index.html')
-      .pipe(include())
-      .pipe(gulp.dest('./dist/'))
+  return gulp.src('./src/index.html')
+             .pipe(include())
+             .pipe(gulp.dest('./dist/'))
 })
 
-gulp.task('server', function() {
+gulp.task('server', ['html'], function() {
   browserSync.init({
     server: {
       baseDir: 'dist'
     }
   })
 
-  gulp.watch('./src/**/*').on('change', browserSync.reload)
+  gulp.watch('./dist/**/*').on('change', browserSync.reload)
 
   gulp.watch('./src/sass/**/*.scss', ['sass'])
+  gulp.watch('./src/**/*.html', ['html'])
 })
